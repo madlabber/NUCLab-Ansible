71b3768412725705fd55a2770dac2374b47465d0
madlabber <madlabber1@gmail.com>
2019-06-09 22:06:14 -0700

    updates

--- make_floppy.sh
+++ make_floppy.sh
@@ -1,19 +1,13 @@
 !/usr/bin/bash.sh
 
 dd if=/dev/zero of=floppy.dmg bs=1k count=1440
-
-partition_table=$'778135908,1141509631,0x72,-,357,116,40,357,32,45\n168689522,1936028240,0x65,-,288,115,43,367,114,50\n1869881465,1936028192,0x79,-,366,32,33,357,32,43\n0,-657740800,0x0D,-,372,97,50,0,10,0'
-
-echo "$partition_table" | fdisk -r -y floppy.dmg
-
 dev=$(hdiutil attach -nomount floppy.dmg)
+newfs_msdos -f 1440 -v floppy $dev
 
-newfs_msdos -v floppy $dev
+#hdiutil detach $dev
 
-hdiutil detach $dev
-
-out=$(hdiutil attach floppy.dmg) 
-dev=$(echo $out | cut -d' ' -f1)
+out=$(hdiutil mountvol $dev) 
+#dev=$(echo $out | cut -d' ' -f1)
 mnt=$(echo $out | cut -d' ' -f2)
 
 echo $dev
