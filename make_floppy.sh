335c36db7f963d2926f9adb7de1ad792d0105a3c
madlabber <madlabber1@gmail.com>
2019-06-10 01:13:17 -0700

    unattend floppy

--- make_floppy.sh
+++ make_floppy.sh
@@ -1,13 +1,12 @@
 !/usr/bin/bash.sh
 
-dd if=/dev/zero of=floppy.dmg bs=1k count=1440
+dd if=/dev/zero of=floppy.flp bs=1k count=1440
 dev=$(hdiutil attach -nomount floppy.dmg)
 newfs_msdos -f 1440 -v floppy $dev
 
-#hdiutil detach $dev
 
 out=$(hdiutil mountvol $dev) 
-#dev=$(echo $out | cut -d' ' -f1)
+
 mnt=$(echo $out | cut -d' ' -f2)
 
 echo $dev
@@ -19,3 +18,9 @@ hdiutil detach $dev
 
 rm floppy.flp 
 mv floppy.dmg floppy.flp
+
+# mkdir unattend
+# cp autounattend.xml unattend
+# hdiutil create -size 1440k -fs "MS-DOS FAT12" -layout NONE -srcfolder unattend -format UDRW -ov floppy.dmg
+# mv floppy.dmg floppy.flp
+
