cb3e250f12fb3d92afa2419a83c6f21a8d00571e
madlabber <madlabber1@gmail.com>
2019-06-12 08:07:37 -0400

    completed dynamic inventory
    inventory file is not longer referenced.  All inventory is dynamically generated from the vars

--- make_floppy.sh
+++ make_floppy.sh
@@ -1,26 +1,26 @@
-!/usr/bin/bash.sh
+#!/bin/bash
 
-dd if=/dev/zero of=floppy.flp bs=1k count=1440
-dev=$(hdiutil attach -nomount floppy.dmg)
-newfs_msdos -f 1440 -v floppy $dev
+# dd if=/dev/zero of=floppy.flp bs=1k count=1440
+# dev=$(hdiutil attach -nomount floppy.dmg)
+# newfs_msdos -f 1440 -v floppy $dev
 
 
-out=$(hdiutil mountvol $dev) 
+# out=$(hdiutil mountvol $dev) 
 
-mnt=$(echo $out | cut -d' ' -f2)
+# mnt=$(echo $out | cut -d' ' -f2)
 
-echo $dev
-echo $mnt
-cp autounattend.xml $mnt
-ls $mnt
+# echo $dev
+# echo $mnt
+# cp autounattend.xml $mnt
+# ls $mnt
 
-hdiutil detach $dev
+# hdiutil detach $dev
 
-rm floppy.flp 
-mv floppy.dmg floppy.flp
-
-# mkdir unattend
-# cp autounattend.xml unattend
-# hdiutil create -size 1440k -fs "MS-DOS FAT12" -layout NONE -srcfolder unattend -format UDRW -ov floppy.dmg
+# rm floppy.flp 
 # mv floppy.dmg floppy.flp
+
+mkdir unattend
+cp autounattend.xml unattend
+hdiutil create -size 1440k -fs "MS-DOS FAT12" -layout NONE -srcfolder unattend -format UDRW -ov floppy.dmg
+mv floppy.dmg floppy.flp
 
