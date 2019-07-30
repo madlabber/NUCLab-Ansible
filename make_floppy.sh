51e14c08cd138aa65a14dde5c39fa9c2f42215e2
madlabber <madlabber1@gmail.com>
2019-07-30 09:52:45 -0700

    misc updates

--- make_floppy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir unattend
-cp autounattend.xml unattend
-hdiutil create -size 1440k -fs "MS-DOS FAT12" -layout NONE -srcfolder unattend -format UDRW -ov floppy.dmg
-mv floppy.dmg floppy.flp
-
